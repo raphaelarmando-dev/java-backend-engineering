c4ed7262b3cbd7fc3fa69da058a250f7ea28fde3
Raphael <raphaelarmando46@gmail.com>
2026-03-29 14:06:11 -0300

    feat: primitive types and numeric limits

--- src/com/raphael/fundamentals/basics/TiposPrimitivos.java
+++ src/com/raphael/fundamentals/basics/TiposPrimitivos.java
@@ -1,4 +1,37 @@
 package com.raphael.fundamentals.basics;
 
 public class TiposPrimitivos {
+    public static void main(String[] args) {
+        // Info de funcionario
+
+        // Tipos numericos inteiros
+        byte anosDeEmpresa = 28;
+        short numeroDeVoos = 452;
+        int id = 82423;
+        long pontosAcumulados = 2146131434L;
+
+        // Tipos numericos reais
+        float salario = 16_425.41F;
+        double vendasAcumuladas = 2_412_923_132.12;
+
+        //Tipo boolean
+        boolean estaDeFerias = false; // true
+
+        // Tipo caractere
+        char status = 'A'; // ativo
+
+        // Dias de Empresa
+        System.out.println(anosDeEmpresa * 365);
+
+        // Numero de viagens
+
+        System.out.println(numeroDeVoos / 2);
+
+        // Pontos por real
+        System.out.println(pontosAcumulados / vendasAcumuladas);
+
+        System.out.println(id + ": ganha -> " + salario);
+        System.out.println("Ferias? " + estaDeFerias);
+        System.out.println("Status: " + status);
+    }
 }
