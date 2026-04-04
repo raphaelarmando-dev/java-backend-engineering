523bb6573885bc6f8505a6048e90ec5bfcd4ef11
Raphael <raphaelarmando46@gmail.com>
2026-04-03 21:01:36 -0300

    feat: add AreaCirc class to practice static final constants

---
+++ src/com/raphael/fundamentals/classe/AreaCirc.java
@@ -0,0 +1,14 @@
+package com.raphael.fundamentals.classe;
+
+public class AreaCirc {
+    double raio;
+    static final double PI = 3.1415;
+
+    AreaCirc(double raioInicial) {
+        raio = raioInicial;
+    }
+
+    double area() {
+        return PI * Math.pow(raio, 2);
+    }
+}
