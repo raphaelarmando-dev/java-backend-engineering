1f233c999072512ff4324cce2cbb409545ac8380
Raphael <raphaelarmando46@gmail.com>
2026-04-04 21:20:25 -0300

    feat: solve static vs instance access challenge

---
+++ src/com/raphael/fundamentals/classe/DesafioPrimeiroTrajeto.java
@@ -0,0 +1,10 @@
+package com.raphael.fundamentals.classe;
+
+public class DesafioPrimeiroTrajeto {
+    int a = 3; // Atributo
+
+    public static void main(String[] args) {
+        DesafioPrimeiroTrajeto d = new DesafioPrimeiroTrajeto();
+        System.out.println(d.a);
+    }
+}
