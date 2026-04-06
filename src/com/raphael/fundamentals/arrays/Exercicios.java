1591acd1b25ce27444cdf38d80c1b6fa3b649df7
Raphael <raphaelarmando46@gmail.com>
2026-04-05 22:09:15 -0300

    feat: implement enhanced for loop to calculate array average

--- src/com/raphael/fundamentals/arrays/Exercicios.java
+++ src/com/raphael/fundamentals/arrays/Exercicios.java
@@ -8,5 +8,11 @@ public class Exercicios {
         notasAlunoA[2] = 9.2;
 
         System.out.println(notasAlunoA[0]);
+
+        double totalAlunoA = 0;
+        for(double nota: notasAlunoA) {
+            totalAlunoA += nota;
+        }
+        System.out.println("Média: " + totalAlunoA / notasAlunoA.length);
     }
 }
