89e63689136ed6d5f0d51629a3ed41b4eb455e63
Raphael <raphaelarmando46@gmail.com>
2026-04-06 22:13:06 -0300

    feat: implement nested loops to traverse Matrix

--- src/com/raphael/fundamentals/arrays/Matriz.java
+++ src/com/raphael/fundamentals/arrays/Matriz.java
@@ -5,5 +5,10 @@ public class Matriz {
         double[][] notasDaTurma = new double[3][3]; // 3 alunos, 3 notas cada
         notasDaTurma[0][0] = 9.9;
         System.out.println("Primeira nota da matriz: " + notasDaTurma[0][0]);
+        for (int a = 0; a < notasDaTurma.length; a++) {
+            for (int n = 0; n < notasDaTurma[a].length; n++) {
+                System.out.printf("Aluno %d, Nota %d: %.1f\n", a, n, notasDaTurma[a][n]);
+            }
+        }
     }
 }
