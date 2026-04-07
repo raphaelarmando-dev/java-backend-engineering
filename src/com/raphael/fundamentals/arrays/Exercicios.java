443103ebd89f14449e1a09c2b16bd4adef749bf7
Raphael <raphaelarmando46@gmail.com>
2026-04-06 22:10:04 -0300

    feat: implement dynamic array with Scanner and average calculation

--- src/com/raphael/fundamentals/arrays/Exercicios.java
+++ src/com/raphael/fundamentals/arrays/Exercicios.java
@@ -1,5 +1,7 @@
 package com.raphael.fundamentals.arrays;
 
+import java.util.Scanner;
+
 public class Exercicios {
     public static void main(String[] args) {
         double[] notasAlunoA = new double[3];
@@ -14,5 +16,26 @@ public class Exercicios {
             totalAlunoA += nota;
         }
         System.out.println("Média: " + totalAlunoA / notasAlunoA.length);
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Quantas notas? ");
+        int qtdeNotas = entrada.nextInt();
+
+        double[] notas = new double[qtdeNotas];
+
+        for (int i = 0; i < notas.length; i++) {
+            System.out.print("Informe a nota " + (i + 1) + ": ");
+            notas[i] = entrada.nextDouble();
+        }
+
+
+        double total = 0;
+        for (double nota : notas) {
+            total += nota;
+        }
+        System.out.println("A média das notas é: " + (total / notas.length));
+
+        entrada.close();
     }
 }
