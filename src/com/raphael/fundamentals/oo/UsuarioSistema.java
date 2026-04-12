cf8367188cfdd00986313e84fb036239c6647830
Raphael <raphaelarmando46@gmail.com>
2026-04-11 22:34:43 -0300

    feat: implement UsuarioSistema using Autenticavel interface for security logic

---
+++ src/com/raphael/fundamentals/oo/UsuarioSistema.java
@@ -0,0 +1,19 @@
+package com.raphael.fundamentals.oo;
+
+public class UsuarioSistema implements Autenticavel {
+    private String login;
+    private String senhaCadastrada = "admin123";
+
+    public UsuarioSistema(String login) {
+        this.login = login;
+    }
+
+    @Override
+    public boolean validarAcesso(String senha) {
+        return this.senhaCadastrada.equals(senha);
+    }
+
+    public void acessar() {
+        System.out.println("Usuário " + login + " logado no sistema.");
+    }
+}
