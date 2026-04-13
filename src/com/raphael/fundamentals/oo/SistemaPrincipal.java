2395bc4ece5844af3940785882a8d4bc4db2e983
Raphael <raphaelarmando46@gmail.com>
2026-04-12 23:21:53 -0300

    feat: complete system orchestration with working logging logic

--- src/com/raphael/fundamentals/oo/SistemaPrincipal.java
+++ src/com/raphael/fundamentals/oo/SistemaPrincipal.java
@@ -1,4 +1,16 @@
 package com.raphael.fundamentals.oo;
 
 public class SistemaPrincipal {
+    public static void main(String[] args) {
+        Notificavel logger = new LogService();
+
+        UsuarioSistema user = new UsuarioAdmin("Raphael_Admin");
+
+        if (user.validarAcesso("admin123")) {
+            user.acessar();
+            logger.enviarLog("Usuário " + user.getLogin() + " entrou no sistema.");
+        } else {
+            logger.enviarLog("Tentativa de login falhou!");
+        }
+    }
 }
