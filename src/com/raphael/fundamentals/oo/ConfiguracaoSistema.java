69c7381cbb1eaac62ea41f49a4abbd8fb7c7683e
Raphael <raphaelarmando46@gmail.com>
2026-04-12 23:12:58 -0300

    feat: create static configuration class with immutability patterns

---
+++ src/com/raphael/fundamentals/oo/ConfiguracaoSistema.java
@@ -0,0 +1,10 @@
+package com.raphael.fundamentals.oo;
+
+public class ConfiguracaoSistema {
+
+    public static final String NOME_SISTEMA = "Alpha Banking Core";
+    public static final int MAX_TENTATIVAS = 3;
+
+    private ConfiguracaoSistema() {
+    }
+}
