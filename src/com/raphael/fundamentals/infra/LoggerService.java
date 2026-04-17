6f17a9dfdbc73fe76c66b4eba09a9c535614361b
Raphael <raphaelarmando46@gmail.com>
2026-04-16 23:22:37 -0300

    feat: create centralized LoggerService for infrastructure logging

---
+++ src/com/raphael/fundamentals/infra/LoggerService.java
@@ -0,0 +1,7 @@
+package com.raphael.fundamentals.infra;
+
+public class LoggerService {
+    public static void logInfo(String mensagem) {
+        System.out.println("[INFO] - " + mensagem);
+    }
+}
