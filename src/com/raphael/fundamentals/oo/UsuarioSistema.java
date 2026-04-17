adbcd14c5dd86fd94aa0a44b3bdda2c5f8446388
Raphael <raphaelarmando46@gmail.com>
2026-04-16 23:21:11 -0300

    feat: add creation timestamp and validation logic to UsuarioSistema

--- src/com/raphael/fundamentals/oo/UsuarioSistema.java
+++ src/com/raphael/fundamentals/oo/UsuarioSistema.java
@@ -1,14 +1,22 @@
 package com.raphael.fundamentals.oo;
 
-public class UsuarioSistema implements Autenticavel {
+import java.time.LocalDateTime;
+
+public class UsuarioSistema implements Autenticavel, ValidadorDados {
     private String login;
     private String senhaCadastrada = "admin123";
+    private LocalDateTime dataCriacao = LocalDateTime.now();
 
     public UsuarioSistema(String login) {
         this.login = login;
     }
+
     public String getLogin() {
         return this.login;
+    }
+
+    public LocalDateTime getDataCriacao() {
+        return this.dataCriacao;
     }
 
     @Override
@@ -16,9 +24,12 @@ public class UsuarioSistema implements Autenticavel {
         return this.senhaCadastrada.equals(senha);
     }
 
+    @Override
+    public boolean isValido() {
+        return this.login != null && this.login.length() >= 3;
+    }
+
     public void acessar() {
         System.out.println("Usuário " + login + " logado no sistema.");
     }
-
-
 }
