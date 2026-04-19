c65535ff7f0ca45f9fefec7ae3a1c0b6bc37c97f
Raphael <raphaelarmando46@gmail.com>
2026-04-18 23:21:42 -0300

    refactor: change password visibility to protected for inheritance

--- src/com/raphael/fundamentals/oo/UsuarioSistema.java
+++ src/com/raphael/fundamentals/oo/UsuarioSistema.java
@@ -4,7 +4,7 @@ import java.time.LocalDateTime;
 
 public class UsuarioSistema implements Autenticavel, ValidadorDados {
     private String login;
-    private String senhaCadastrada = "admin123";
+    protected String senhaCadastrada = "admin123";
     private LocalDateTime dataCriacao = LocalDateTime.now();
 
     public UsuarioSistema(String login) {
