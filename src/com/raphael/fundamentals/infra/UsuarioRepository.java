505961c6c8635696f728cb9fad2bed341d4cee22
Raphael <raphaelarmando46@gmail.com>
2026-04-20 23:50:29 -0300

    feat: integrate Notificador into UsuarioRepository persistence flow

--- src/com/raphael/fundamentals/infra/UsuarioRepository.java
+++ src/com/raphael/fundamentals/infra/UsuarioRepository.java
@@ -2,17 +2,24 @@ package com.raphael.fundamentals.infra;
 
 import com.raphael.fundamentals.oo.UsuarioSistema;
 import com.raphael.fundamentals.oo.UsuarioDataHandler;
+import com.raphael.fundamentals.oo.Notificador;
 import java.util.List;
 
 public class UsuarioRepository extends RepositorioBase<UsuarioSistema>
         implements UsuarioDataHandler, CrudRepository<UsuarioSistema> {
+
+
+    private Notificador notificador = new EmailNotificador();
 
     @Override
     public void persistir(UsuarioSistema usuario) throws PersistenciaException {
         if (usuario.getLogin() == null || usuario.getLogin().isEmpty()) {
             throw new PersistenciaException("Login inválido para persistência.");
         }
+
         super.inserir(usuario);
+
+        notificador.enviar("Usuário " + usuario.getLogin() + " persistido com ID: " + usuario.getId());
     }
 
     @Override
