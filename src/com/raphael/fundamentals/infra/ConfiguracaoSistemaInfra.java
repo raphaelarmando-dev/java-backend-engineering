37810bdb3d52869b99284fb23fc22e4f53f5c2b9
Raphael <raphaelarmando46@gmail.com>
2026-04-23 22:36:53 -0300

    fix: inject UsuarioRepository into UsuarioService in MainServiceTest and add getAmbiente getter to ConfiguracaoSistemaInfra

--- src/com/raphael/fundamentals/infra/ConfiguracaoSistemaInfra.java
+++ src/com/raphael/fundamentals/infra/ConfiguracaoSistemaInfra.java
@@ -2,13 +2,18 @@ package com.raphael.fundamentals.infra;
 
 public class ConfiguracaoSistemaInfra {
     private static ConfiguracaoSistemaInfra instancia;
+
     public final String VERSAO = "1.0.0-STABLE";
-    public final String AMBIENTE = "DESENVOLVIMENTO";
+    private final String AMBIENTE = "DESENVOLVIMENTO";
 
     private ConfiguracaoSistemaInfra() {}
 
     public static ConfiguracaoSistemaInfra getInstancia() {
         if (instancia == null) instancia = new ConfiguracaoSistemaInfra();
         return instancia;
+    }
+
+    public String getAmbiente() {
+        return AMBIENTE;
     }
 }
